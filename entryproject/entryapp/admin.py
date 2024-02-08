cdc1595a4c81ec08fe1fb6ded38ade37bb3d0503
yymasaki <y.masaki34@icloud.com>
2024-02-08 20:28:37 +0900

    作業途中でプッシュ

---
+++ entryproject/entryapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import EntryModel
+
+admin.site.register(EntryModel)
---
+++ entryproject/entryapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EntryappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'entryapp'
